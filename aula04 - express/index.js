4085059c42f2db7a23cf364fa2e03d63e406a80a
Marlon Almino <marlonalmino@hotmail.com>
2021-08-03 10:57:48 -0300

    Adicionado body-parser e utilizando com post

--- aula04 - express/index.js
+++ aula04 - express/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
 
 // Config
@@ -8,6 +9,10 @@ const Sequelize = require('sequelize')
     // Template Engine
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+
+    //Body Parser
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
 
     // Conexão com o banco de dados MySql
     const sequelize = new Sequelize('test', 'root', 'admin', {
@@ -18,6 +23,10 @@ const Sequelize = require('sequelize')
 // Rotas
     app.get('/cad', function(req, res) {
         res.render('formulario')
+    })
+
+    app.post('/add', function(req, res) {
+        res.send(`Texto: ${req.body.titulo}<br>Conteúdo: ${req.body.conteudo}`)
     })
 
 app.listen(8081, function(){
